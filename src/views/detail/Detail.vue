219937b24de89edafd6c85596eeadeceb0c016e4
LIUJIAYI <liujiayiii@qq.com>
2019-07-12 13:56:34 +0800

    0.8

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <DetailBanner/>
+    <DetailHeader/>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
   import DetailBanner from './components/Banner'
+  import DetailHeader from './components/Header'
 
   export default {
     name: "Detail",
-    components: {DetailBanner}
+    components: {DetailBanner, DetailHeader}
   }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
